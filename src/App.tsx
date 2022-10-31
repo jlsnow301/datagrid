a00689837e8f8fd7ae05472b3234d67a30484dc4
Jeremiah <jlsnow301@protonmail.com>
2022-10-31 00:52:45 -0700

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { PageConstructor } from "./PageConstructor";
 import data from "./data.json";
 
 export const App = () => {
+  // Let's make a one line page creator!
+
   return (
     <div className="h-96 bg-slate-600">
       <PageConstructor data={data} />
